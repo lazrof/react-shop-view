1288c0c409ee01f485ee9d6dcd480b3f3acb1013
Nunzio Ruffo <nunzio.ruffo@sigma3ds.com>
2020-04-06 23:02:10 -0500

    change everything to react hooks, and added some styles

--- src/App.js
+++ src/App.js
@@ -1,58 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import NavBar from './components/NavBar';
 import ProductItem from './components/ProductItem';
 import './styles/custom/global.css';
 import MiniCart from './components/MiniCart';
+import shopData from './shop-data.json';
 
-class App extends Component {
 
-	constructor() {
-		super();
-		this.state = {}
-		//this.handleAddToDo = this.handleAddToDo.bind(this);
+const App = () => {
+	
+	const [state, setState] = useState(shopData);
 
-	}
 
-	render() {
+	
 
-		return (
-			<div className="app">
-				<NavBar/>
-				<div className="container">
-					<div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-						<h1 className="display-4">Albums</h1>
-						<p className="lead">Best albums ever made.</p>
-					</div>
+	return (
+		<div className="app">
+			<NavBar/>
+			<div className="container">
+				<div className="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+					<h1 className="display-4">Albums</h1>
+					<p className="lead">Best albums ever made.</p>
+					{/* <p>You clicked {count} times</p> */}
+					{/* <button onClick={() => setCount(count + 1)}>Click me!</button> */}
 				</div>
-
-				<div className="container pb-5">
-					<div className="row">
-						<ProductItem />
-						<ProductItem />
-						<ProductItem />
-						<ProductItem />
-						<ProductItem />
-						<ProductItem />
-						<ProductItem />
-						<ProductItem />
-					</div>
-				</div>
-
-				<div className="container">
-					<footer className="pt-4 my-md-2 pt-md-4 border-top">
-						<div className="row">
-							<div className="col-12">
-								<small className="d-block mb-3 text-muted text-center">&copy; React Ecommerce 2020</small> 
-							</div>
-						</div>
-					</footer>
-				</div>
-
-				<MiniCart />
-
 			</div>
-		)
-	}
+
+
+			<div className="container pb-5">
+				<div className="row">
+					{state.items.map(item => <ProductItem  itemData={item}/>)}
+				</div>
+			</div>
+
+			<div className="container">
+				<footer className="pt-4 my-md-2 pt-md-4 border-top">
+					<div className="row">
+						<div className="col-12">
+							<small className="d-block mb-3 text-muted text-center">&copy; React Ecommerce 2020</small> 
+						</div>
+					</div>
+				</footer>
+			</div>
+
+			<MiniCart />
+
+		</div>
+	)
 }
 
 export default App;
--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,10 +1,23 @@
 import React from 'react';
 
-const ProductItem = () =>  {
+const ProductItem = (props) =>  {
+    console.log(props.itemData);
+    const { name, price, type } = props.itemData;
     return(
         <div className="col-md-4">
             <div className="item py-3">
-                <img className="img-fluid" src={require('../img/daft-punk.jpg')} alt="" />
+                {/* <img className="img-fluid" src={require('../img/daft-punk.jpg')} alt="" /> */}
+                
+                <img className="img-fluid" src= "https://i0.wp.com/marvin.com.mx/wp-content/uploads/2019/08/Black-Album-1.jpg?resize=759%2C500&ssl=1" alt="" />
+                <div>
+                    <div className="product-info">
+                        <p class="info">{name} <span className="price"> ${price}</span></p>
+                        <p className="add-to-cart">Add to Cart</p>
+                    </div>
+                    <div>
+                        <p className="product-cat">{type}</p>
+                    </div>
+                </div>
             </div>
         </div>
     )
