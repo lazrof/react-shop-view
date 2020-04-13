516ae8d845febe96ab796ce8f4db73ff73907a72
lazrof <nunzio_ruffo@hotmail.com>
2020-04-12 21:52:08 -0500

    Initial build subtree commit

---
+++ build/precache-manifest.de71f7c388af6d5d91622d8fa0f6ab29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cda63d365df5f65b93c6557efe6f55a",
+    "url": "/react-shop-view/index.html"
+  },
+  {
+    "revision": "fbf20fecbde14db76d53",
+    "url": "/react-shop-view/static/css/main.9010adcb.chunk.css"
+  },
+  {
+    "revision": "e921a2bcde6b19e16e18",
+    "url": "/react-shop-view/static/js/2.2ac2d041.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-shop-view/static/js/2.2ac2d041.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbf20fecbde14db76d53",
+    "url": "/react-shop-view/static/js/main.202eff13.chunk.js"
+  },
+  {
+    "revision": "525c3371cc562afce6c9",
+    "url": "/react-shop-view/static/js/runtime-main.141dcdf1.js"
+  },
+  {
+    "revision": "2635300379dd62260f5d0dee2f21c672",
+    "url": "/react-shop-view/static/media/daft-punk.26353003.jpg"
+  }
+]);
